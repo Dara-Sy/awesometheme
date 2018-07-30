1a82a050451481559f6965e163878743241501d0
Dara-Sy <darajanne@gmail.com>
2018-07-29 22:12:38 -0400

    fixing indentations

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ function awesome_script_enqueue() {
     ========================================================================================
 
 */
-add_action('wp_enqueue_scripts', 'awesome_script_enqueue');
+    add_action('wp_enqueue_scripts', 'awesome_script_enqueue');
 
 /*
 
@@ -71,14 +71,14 @@ function awesome_theme_setup() {
 */
 
 
-add_action('init', 'awesome_theme_setup');
-// add_action('after_setup_theme', 'awesome_theme_setup');
+    add_action('init', 'awesome_theme_setup');
+    // add_action('after_setup_theme', 'awesome_theme_setup');
 
-add_theme_support('custom-background');
-add_theme_support('custom-header');
-add_theme_support('post-thumbnails');
+    add_theme_support('custom-background');
+    add_theme_support('custom-header');
+    add_theme_support('post-thumbnails');
 
-add_theme_support('post-formats', array('aside', 'image', 'video'));
+    add_theme_support('post-formats', array('aside', 'image', 'video'));
 
 ?>
 
