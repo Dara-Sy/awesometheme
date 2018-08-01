6c515934d436d4401d6b693c2a8f2f4f9a8320a8
Dara-Sy <darajanne@gmail.com>
2018-07-31 20:58:36 -0400

    changes to content.php

--- content.php
+++ content.php
@@ -1,9 +1,18 @@
-<h3><?php the_title(); ?></h3>
-<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?> </div>
-<!-- F j Y is the Month day and Year -->
-<!-- g:i a means hour:minutes am or pm -->
-<small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<p><?php the_content(); ?></p>
+    <header class="entry-header">
 
-<br>
+        <!-- sprintf() is a method in PHP -->
+        <!-- %s is replaced... get_permalink gets the link... esc_url prevents injections -->
+        <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+        <!-- F j Y is the Month day and Year -->
+        <!-- g:i a means hour:minutes am or pm -->
+        <small>Posted on: <?php the_time('F j, Y');?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+
+    </header>
+
+    <div class="thumbnail-img"><?php the_post_thumbnail('full'); ?></div>
+
+    <p><?php the_content(); ?></p>
+
+</article>
